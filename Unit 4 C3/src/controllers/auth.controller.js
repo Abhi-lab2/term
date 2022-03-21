1d22bc734e2b19ab80967e136305c829ca9d333e
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-03-21 11:02:11 +0530

    Updte

--- Unit 4 C3/src/controllers/auth.controller.js
+++ Unit 4 C3/src/controllers/auth.controller.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 const { findOne } = require("../models/user.model");
 
 // function authenticateToken(req, res, next) {
+    
 //   const authHeader = req.headers['authorization']
 //   const token = authHeader && authHeader.split(' ')[1]
 
