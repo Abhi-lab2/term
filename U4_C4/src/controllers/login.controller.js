c9f708d052a18343da7eaf9c31e9c6c0c81d7d61
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-03-28 11:01:20 +0530

    updated

--- U4_C4/src/controllers/login.controller.js
+++ U4_C4/src/controllers/login.controller.js
@@ -1,14 +1,41 @@
 const express = require("express");
-
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
 const User = require("../models/user.model");
 
+const upload = require("../middleware/upload");
+
 router.post(
   "",
-  body("email ").isEmail().withMessage("invalid Email"),
-  body("password  ").isEmail().withMessage("Worng password ")
+  body("email").isEmail().withMessage("Email is invalid"),
 
-  
+  body("password").not().isEmpty().withMessage("Password is required"),
+
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email: email });
+
+      if (!user) {
+        return res.status(500).send({ error: "Wrong Email or Password" });
+      }
+
+      if (user.password == password) {
+        return res.status(200).send("abcs your signin is successfull!");
+      } else {
+        return res.status(500).send({ error: "Hey, Wrong Email or Password" });
+      }
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  }
 );
+
+module.exports = router;
--- U4_C4/src/middleware/upload.js
+++ U4_C4/src/middleware/upload.js
@@ -1,1 +1,39 @@
-// Sample file at beginning.
+const path = require("path");
+
+const multer = require("multer");
+// file filter .
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(
+      null, //error as null
+      path.join(__dirname, "./uploads")
+    );
+  },
+  filename: function (req, file, cb) {
+    console.log({ file });
+    const uniquePrefix = Date.now();
+    cb(null, uniquePrefix + "." + file.originalname);
+  },
+});
+
+function fileFilter(req, file, cb) {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype == "image/jpg" ||
+    file.mimetype == "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Incorrrect type of file"));
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+});
+
+module.exports = upload;
