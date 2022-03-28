3efde4c6c7ab4ef2d5f7cda821025f40788df0d0
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-03-28 11:05:17 +0530

    update

--- U4_C4/src/index.js
+++ U4_C4/src/index.js
@@ -5,10 +5,13 @@ const app = express();
 // making controllers
 const todoController = require("./controllers/todo.controller");
 const userController = require("./controllers/user.controller");
+const registerController = require("./controllers/register.controller");
+const loginController = require("./controllers/login.controller.js");
 
 app.use(express.json());
 
 app.use("/todos", todoController);
 app.use("/user", userController);
-
+app.use("/register", registerController);
+app.use("/login", loginController);
 module.exports = app;
